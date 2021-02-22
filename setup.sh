b26cca769e4d6cc39af5569a9c6e1abd894512d1
Eric Raio <eraio@ezoic.com>
2021-02-22 09:38:07 -0800

    Setting up setup script cron jobs

--- setup.sh
+++ setup.sh
@@ -3,3 +3,11 @@
 mkdir -p ~/.config/scripts/
 
 mv evernote/evernote-things-sync.applescript ~/.config/scripts/evernote-things-sync.applescript
+
+#write out current crontab
+crontab -l > evernote-cron
+#echo new cron into cron file
+echo "* * * * *  osascript ~/.config/scripts/evernote-things-sync.applescript" >> evernote-cron
+#install new cron file
+crontab evernote-cron
+rm evernote-cron
